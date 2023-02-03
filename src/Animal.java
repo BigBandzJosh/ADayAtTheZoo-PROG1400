e97d43fe4fad03602a0f2dae7c09e7da0e64f63f
Josh <113129413+BigBandzJosh@users.noreply.github.com>
2023-02-03 13:50:29 -0400

    Animals and stuff!

---
+++ src/Animal.java
@@ -0,0 +1,32 @@
+public class Animal {
+    private String species;
+    private double MaxWeight;
+    private String habitat;
+    boolean isEndangered;
+    public Animal() {
+        this.species = "Dog";
+        this.MaxWeight = 60;
+        this.habitat = "Under my feet";
+        this.isEndangered = false;
+    }
+    public Animal (String species, double MaxWeight, String habitat, boolean isEndangered) {
+        this.species = species;
+        this.MaxWeight = MaxWeight;
+        this.habitat = habitat;
+        this.isEndangered = isEndangered;
+    }
+    public String fedAnimal(String meal) {
+        return "The " + species + " ate " + meal;
+    }
+    public String animalSounds(String sound) {
+        return "The " + species + " makes the sound " + sound;
+    }
+    public String toString() {
+        if (!isEndangered) {
+            return "The " + species + " is a " + habitat + " animal that weighs " + MaxWeight + " pounds.";
+        }
+        else {
+            return "The " + species + " is a " + habitat + " animal that weighs " + MaxWeight + " pounds. It is endangered.";
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal1 = new Animal("Elephant", 1000, "desert", false);
+        Animal animal2 = new Animal("Lion", 500, "desert", true);
+        Animal animal3 = new Animal("Tiger", 800, "desert", true);
+        Animal animal4 = new Animal();
+        Animal[] animals = {animal1, animal2, animal3,animal4};
+        for (int i =0; i < animals.length; i++) {
+            System.out.println(animals[i]);
+        }
+    }
+}
